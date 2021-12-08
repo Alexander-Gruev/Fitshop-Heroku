c78d688a8d0b97fa66312f472a21835825810631
Alexander Gruev <alexandergruev2000@gmail.com>
2021-12-08 18:35:12 +0200

    added @Transactional to UserRepository

--- src/main/java/com/example/fitshop/repository/UserRepository.java
+++ src/main/java/com/example/fitshop/repository/UserRepository.java
@@ -4,9 +4,11 @@ import com.example.fitshop.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByUsername(String username);
