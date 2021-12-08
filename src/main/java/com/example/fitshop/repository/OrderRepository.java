f7c6de8d78f68aaa5366bc554e470b91ac3293d0
Alexander Gruev <alexandergruev2000@gmail.com>
2021-12-08 18:24:52 +0200

    added @Transactional to OrderRepository to fix bug

--- src/main/java/com/example/fitshop/repository/OrderRepository.java
+++ src/main/java/com/example/fitshop/repository/OrderRepository.java
@@ -4,9 +4,11 @@ import com.example.fitshop.model.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
+@Transactional
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
 
     List<OrderEntity> findAllByClient_Username(String username);
